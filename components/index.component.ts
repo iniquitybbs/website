71e2f1c8373d8708833691cf2ee875766d184c60
Dan Stephenson <ispyhumanfly@gmail.com>
2018-09-09 19:58:45 -0400

    New blog post, though, not really finished.

--- components/index.component.ts
+++ components/index.component.ts
@@ -10,21 +10,12 @@ new Vue({
     data () {
         return {
             show_broadcasts: true,
-            navigation: [
-                {
-                    title: "Index",
-                    url: "/index"
-                },
-                {
-                    title: "Platform",
-                    url: "/platform"
-                },
-                {
-                    title: "Account",
-                    url: "/account"
-                }
-            ],
             posts: [
+                {
+                    title: 'Documentation, 0.0.19 released, and Smooth has joined development team!',
+                    author: 'ispyhumanfly +o euphoriabbs.org',
+                    content: "I’ll admit there’s been a bit of a lull since my initial creative spree with Iniquity BBS. The first few weeks were on fire, but between December and February not much action. Now, I don’t consider 0.0.12 to be a major release at all. In fact, there’s not a whole lot more to it with respect to the previous few releases. But it’s a step in the right direction. Considerable effort was placed on the build process, and, the ease of installation of Iniquity BBS on both Windows and OSX. Future releases will begin to formalize the IPM package management a bit more, as well as focus on being able to login and create accounts."
+                },
                 {
                     title: 'Iniquity BBS 0.0.12 has been released to RubyGems!',
                     author: 'ispyhumanfly +o euphoriabbs.org',
